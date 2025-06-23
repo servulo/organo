dfe6fc5db18356a6c63d2e01ae067bc22f6f2477
Sérvulo Pires da Rocha Júnior <servulo.jr@gmail.com>
2025-06-23 00:17:59 -0300

    Criação do componente livro

---
+++ src/app/componentes/livro/livro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livro',
+  imports: [],
+  templateUrl: './livro.component.html',
+  styleUrl: './livro.component.css'
+})
+export class LivroComponent {
+
+}
